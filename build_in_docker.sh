57726ed5f02e88f458b11d65cbc1558c965c657b
dcflachs <dcflachs@users.noreply.github.com>
2021-10-02 18:05:47 -0400

    Sketching in plugin

--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-
-docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e OUTPUT_FOLDER="/mnt/output" -v $PWD/source:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh $UI_VERSION_LETTER
+[ -z "$COMPOSE_VERSION" ] && COMPOSE_VERSION=2.0.1
+docker run --rm --tmpfs /tmp -v $PWD/archive:/mnt/output:rw -e COMPOSE_VERSION=$COMPOSE_VERSION -e OUTPUT_FOLDER="/mnt/output" -v $PWD/source:/mnt/source:ro vbatts/slackware:latest /mnt/source/pkg_build.sh $UI_VERSION_LETTER
---
+++ source/pkg_build.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+[ -z "$OUTPUT_FOLDER" ] && echo "Output Folder not set" && exit 1
+[ -z "$COMPOSE_VERSION" ] && echo "Compose Version not set" && exit 1
+tmpdir=/tmp/tmp.$(( $RANDOM * 19318203981230 + 40 ))
+version=$(date +"%Y.%m.%d")$1
+
+# mkdir -p $tmpdir/usr/local/emhttp/plugins/compose.manager
+
+# cp -RT /mnt/source/docker.compose/ $tmpdir/usr/local/emhttp/plugins/compose.manager/
+
+mkdir -p $tmpdir/usr/local/lib/docker/cli-plugins/
+cd $tmpdir/usr/local/lib/docker/cli-plugins/
+curl -L -O  https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-linux-x86_64
+curl -L -O  https://github.com/docker/compose/releases/download/v${COMPOSE_VERSION}/docker-compose-linux-x86_64.sha256
+sha256sum -c SHA256SUMS 2>&1 | grep -q OK || exit 2
+rm docker-compose-linux-x86_64.sha256
+
+cd $tmpdir
+
+chmod -R +x $tmpdir/usr/local/lib/docker/cli-plugins/
+
+makepkg -l y -c y $OUTPUT_FOLDER/docker-compose-package-${version}.txz
+
+cd /
+
+rm -rf $tmpdir
+
+echo "MD5:"
+
+md5sum $OUTPUT_FOLDER/docker-compose-package-${version}.txz
