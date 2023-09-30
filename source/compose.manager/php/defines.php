d94e67d9e3904519ad8a3026fa56d47e4b484c3f
dcflachs <dcflachs@gmail.com>
2023-09-29 20:20:26 -0400

    Add PROJECTS_FOLDER to default.cfg

--- source/compose.manager/php/defines.php
+++ source/compose.manager/php/defines.php
@@ -1,5 +1,11 @@
 <?php
-$compose_root = "/boot/config/plugins/compose.manager/projects";
+require_once("/usr/local/emhttp/plugins/dynamix/include/Wrappers.php");
+
+function locate_compose_root($name) {
+    $cfg = parse_plugin_cfg($name);
+    return $cfg['PROJECTS_FOLDER'] ?? "/boot/config/plugins/compose.manager/projects";
+}
+
 $plugin_root = "/usr/local/emhttp/plugins/compose.manager/";
 $socket_name = "compose_manager_action";
 $sName = "compose.manager";
@@ -8,4 +14,5 @@ $docker_label_icon = "net.unraid.docker.icon";
 $docker_label_webui = "net.unraid.docker.webui";
 $docker_label_shell = "net.unraid.docker.shell";
 $docker_label_managed_name = "composeman";
+$compose_root = locate_compose_root($sName);
 ?>
