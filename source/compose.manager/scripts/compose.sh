246a9645e0a2abc1b8d269336fc5eef96dd0cf3e
mtongnz <mtongnz@gmail.com>
2024-01-03 16:04:40 +1300

    Update unRaid's built in image version database to updates show correctly in UI

--- source/compose.manager/scripts/compose.sh
+++ source/compose.manager/scripts/compose.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 export HOME=/root
 
+phpScriptDir=/usr/local/emhttp/plugins/compose.manager/php
+
 SHORT=e:,c:,f:,p:,d:,o:,g:
 LONG=env,command:,file:,project_name:,project_dir:,override:,profile:,debug,recreate
 OPTS=$(getopt -a -n compose --options $SHORT --longoptions $LONG -- "$@")
@@ -132,6 +134,9 @@ case $command in
       fi
       eval docker rmi ${images[*]}
     fi
+    
+    # Update unRaid's local/remote image versions database so GUI shows correct info about updates
+    docker compose -p "$name" ps --format "{{.Image}}" | php $phpScriptDir/DockerUpdate.php 2>&1
     ;;
 
   stop)
