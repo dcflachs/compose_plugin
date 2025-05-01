3910453454e6abb62fa53b9b1192b01dfddbf1f8
dcflachs <dcflachs@users.noreply.github.com>
2025-04-30 21:37:03 -0400

    Change install location for the docker compose cli plugin

--- source/pkg_build.sh
+++ source/pkg_build.sh
@@ -30,9 +30,9 @@ wget --no-check-certificate https://github.com/docker/compose/releases/download/
 sha256sum -c docker-compose-linux-x86_64.sha256 2>&1 | grep -q OK || exit 4
 rm docker-compose-linux-x86_64.sha256
 
-mkdir -p $tmpdir/usr/local/lib/docker/cli-plugins/
-cp docker-compose-linux-x86_64 $tmpdir/usr/local/lib/docker/cli-plugins/docker-compose
-chmod -R +x $tmpdir/usr/local/lib/docker/cli-plugins/
+mkdir -p $tmpdir/usr/lib/docker/cli-plugins/
+cp docker-compose-linux-x86_64 $tmpdir/usr/lib/docker/cli-plugins/docker-compose
+chmod -R +x $tmpdir/usr/lib/docker/cli-plugins/
 rm docker-compose-linux-x86_64
 
 #Install compose switch
